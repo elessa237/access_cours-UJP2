13349df6b70ee2c51f414f9972cfb285626d0312
Maxime <elessamaxime@icloud.com>
2021-12-15 23:08:07 +0100

    [feat] : Infrastructure\migrations
    Signed-off-by: Maxime <elessamaxime@icloud.com>

--- src/Infrastructure/migrations/Version20211127225540.php
+++ src/Infrastructure/migrations/Version20211127225540.php
@@ -2,7 +2,8 @@
 
 declare(strict_types=1);
 
-namespace DoctrineMigrations;
+namespace App\Infrastructure\migrations;
+
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
