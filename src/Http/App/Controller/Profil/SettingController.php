719a94ce08effe10fbc96b0cc651ec35e71dd9e8
Maxime <elessamaxime@icloud.com>
2022-01-30 18:28:01 +0100

    :fire: [remove] : remove code and file
    Signed-off-by: Maxime <elessamaxime@icloud.com>

--- src/Http/App/Controller/Profil/SettingController.php
+++ src/Http/App/Controller/Profil/SettingController.php
@@ -35,58 +35,13 @@ class SettingController extends AbstractController
     /**
      * @Route("/profil/setting/{id}", name="app_profil_setting")
      * @param Utilisateur $user
-     * @param Request $request
      * @return Response
      */
-    public function index(Utilisateur $user, Request $request) : Response
+    public function index(Utilisateur $user) : Response
     {
-        $userDto = new UtilisateurDto($user);
-
-        $userDto->email = "";
-        $generalSetting = $this->createForm(GeneralSettingType::class, $userDto);
-        $emailSetting = $this->createForm(EmailSettingType::class, $userDto);
-        $passwordSetting = $this->createForm(PasswordSettingType::class, $userDto);
-
-        $this->generalSetting($generalSetting, $userDto, $request);
-        $this->emailSetting($emailSetting, $userDto, $request);
-        $this->passwordSetting($passwordSetting, $userDto, $request);
-
-        return $this->render("profil/setting.html.twig",[
-            "generalSetting" => $generalSetting->createView(),
-            "emailSetting" => $emailSetting->createView(),
-            "passwordSetting" => $passwordSetting->createView(),
+        return $this->render("profil/setting.html.twig", [
+            "user" => $user
         ]);
-    }
-
-    private function generalSetting(FormInterface $form, UtilisateurDto $utilisateurDto, Request $request)
-    {
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            $this->command->updateGeneralSetting($utilisateurDto);
-            return $this->redirectToRoute("app_profil_setting", ["id" => $utilisateurDto->id]);
-        }
-        return false;
-    }
-
-    private function emailSetting(FormInterface $form, UtilisateurDto $utilisateurDto, Request $request)
-    {
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            $this->command->updateEmail($utilisateurDto);
-            return $this->redirectToRoute("app_profil_setting", ["id" => $utilisateurDto->id]);
-        }
-        return false;
-    }
-
-    private function passwordSetting(FormInterface $form, UtilisateurDto $utilisateurDto, Request $request)
-    {
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            $this->command->updatePassword($utilisateurDto);
-        }
     }
 
 }
